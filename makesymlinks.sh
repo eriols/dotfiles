d864a70246b7e5d6597db9588b44b546e7357c1d
Erik Olsson <olsson@spotify.com>
2015-02-18 14:33:34 +0100

    Update tmux.conf to handle statusbar colours on Linux

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -5,5 +5,5 @@ files="bashrc vimrc bash_aliases ghc vimperatorrc vimrc tmux.conf"
 cd ~
 for file in $files; do
 	echo "creating symlink to $file in home dir"
-	ln -s $dir/$file .$file
+	ln -sfn $dir/$file .$file
 done
