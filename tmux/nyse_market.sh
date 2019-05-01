aa691949d7dd4214c03fc2ceb3132c0ca1aa05c6
Erik Olsson <erik.eo.olsson@gmail.com>
2019-05-01 21:47:54 +0200

    Fix typo in market script

--- tmux/nyse_market.sh
+++ tmux/nyse_market.sh
@@ -3,7 +3,7 @@
 dow=$(date +%u)
 hhmm=$(date +%H%M)
 if (($dow < 6 && $hhmm > 1529 && $hhmm < 2200)); then
-    ./~/code/dotfiles/tmux/spot_price
+    ~/code/dotfiles/tmux/spot_price
 else
     echo "NYSE closed"
 fi
